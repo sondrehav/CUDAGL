4da59d3af4225374c94de2692c44d74be1596235
Sondre Havellen <sondre.havellen@gmail.com>
2017-11-07 18:02:05 +0100

    Running CUDA and OpenGL

---
+++ CUDAGL/src/hello_cuda.cu
@@ -0,0 +1,62 @@
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+#include "test.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define N 128
+
+#define CHND(x) \
+	if(x != cudaSuccess) {\
+		fprintf(stderr, "CUDA failed at line %d in file %s!", __LINE__, __FILE__);\
+		exit(-1);\
+	}\
+
+__global__ void add(float *a, float *b, float *out)
+{
+	int thread_id = blockIdx.x;
+	if (thread_id < N)
+	{
+		out[thread_id] = a[thread_id] + b[thread_id];
+	}
+}
+
+int test()
+{
+	srand(time(NULL));
+	float a[N], b[N], out[N];
+	float* dev_a, *dev_b, *dev_out;
+
+	CHND(cudaMalloc((void**)&dev_a, N * sizeof(float)));
+	CHND(cudaMalloc((void**)&dev_b, N * sizeof(float)));
+	CHND(cudaMalloc((void**)&dev_out, N * sizeof(float)));
+
+	for (int i = 0; i < N; i++)
+	{
+		a[i] = (float)rand() / (float)RAND_MAX;
+		b[i] = (float)rand() / (float)RAND_MAX;
+	}
+
+	CHND(cudaMemcpy(dev_a, a, N * sizeof(float), cudaMemcpyHostToDevice));
+	CHND(cudaMemcpy(dev_b, b, N * sizeof(float), cudaMemcpyHostToDevice));
+
+	add<<<N, 1>>>(dev_a, dev_b, dev_out);
+
+	CHND(cudaMemcpy(out, dev_out, N * sizeof(float), cudaMemcpyDeviceToHost));
+
+	printf("[ ");
+	for (int i = 0; i < N; i++)
+	{
+		printf("%3.3f, ", out[i]);
+	}
+	printf(" ]\n");
+
+	cudaFree(dev_a);
+	cudaFree(dev_b);
+	cudaFree(dev_out);
+
+	return 0;
+
+}
--- CUDAGL/src/main.cpp
+++ CUDAGL/src/main.cpp
@@ -1,4 +1,5 @@
 #include <GLFW/glfw3.h>
+#include "test.h"
 
 int main(void)
 {
@@ -18,6 +19,8 @@ int main(void)
 
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
+
+	test();
 
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
